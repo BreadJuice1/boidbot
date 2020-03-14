60b0f8996f927bd534607e9df5f47b3d4c4a3879
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2020-03-13 20:00:21 -0700

    u

--- cogs/admin.py
+++ cogs/admin.py
@@ -19,12 +19,9 @@ class admin(commands.Cog):
 
         @client.command()
         async def clear(self, ctx, amount=None):
-            if amount == None:
-                await ctx.send('please specify an amount')
-            else if amount != None:
-                await ctx.purge(limit=amount)
-                await ctx.send(f'{amount} messages purged')
-                print(f'{amount} messages purged')
+            await ctx.purge(limit=amount)
+            await ctx.send(f'{amount} messages purged')
+            print(f'{amount} messages purged')
 
 def setup(client):
     client.add_cog(admin(client))
