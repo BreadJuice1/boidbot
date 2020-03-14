4b308a40067866627f0ea7531e7525fb81428634
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2020-03-13 18:38:29 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 client = commands.Bot(command_prefix = 'hey pleboid, ')
 
@@ -9,10 +10,20 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} is a friend of bigfoot')
+    print(f'{member} has joined')
 
 @client.event
 async def on_member_remove(member):
-    print(f'{member} is a Neil Patrick Harris lookalike')
+    print(f'{member} has left')
+
+@client.command()
+async def bruh(ctx):
+    await ctx.send(f'Bruh! {round(client.latency * 1000)}ms')
+
+@client.command(aliases=['test'])
+async def breh(ctx, *, question):
+    responses = ['ple', 'boid', 'pleboid']
+    await ctx.send(f'{question}\n{random.choice(responses)}')
+    
 
 client.run('Njg4MTY4MDYyMjI2MjY4MTcz.XmwYgw.BsMbcjd4r3hKwunqS5mx2q0pnyY')
